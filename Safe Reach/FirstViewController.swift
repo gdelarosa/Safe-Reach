0fe5db45c9b7e68196583f34463fb67ad4b3c32f
Gina De La Rosa <g26delarosa@gmail.com>
2016-10-23 21:27:42 -0700

    Uploading new project

---
+++ Safe Reach/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Safe Reach
+//
+//  Created by Gina De La Rosa on 10/23/16.
+//  Copyright © 2016 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Safe Reach/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Safe Reach
+//
+//  Created by Gina De La Rosa on 10/23/16.
+//  Copyright © 2016 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
