e4baea5a55c88c3cac9f732f88fcc933c9a4d553
Gina De La Rosa <g26delarosa@gmail.com>
2016-10-23 22:19:58 -0700

    Added map and json files

---
+++ Safe Reach/MapViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Safe Reach
+//
+//  Created by Gina De La Rosa on 9/6/16.
+//  Copyright © 2016 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var foods = [Food]()
+    var locationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        let initialLocation = CLLocation(latitude: 34.0522, longitude: -118.2437) // LA 
+        centerMapOnLocation(initialLocation)
+        loadInitialDtata()
+        mapView.addAnnotations(foods)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+    }
+    
+    let regionRadius: CLLocationDistance = 10000
+    func centerMapOnLocation(_ location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func loadInitialDtata(){
+        let filename = Bundle.main.path(forResource: "Food", ofType: "json")
+        
+        var data: Data?
+        do{
+            data = try Data(contentsOf: URL(fileURLWithPath: filename!), options: NSData.ReadingOptions(rawValue: 0))
+        }catch _ {
+            data = nil
+        }
+        
+        var jsonObject: AnyObject?
+        
+        if let data = data {
+            do {
+                jsonObject = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0)) as AnyObject 
+            }catch _ {
+                jsonObject = nil
+            }
+        }
+        
+        if let jsonObject = jsonObject as? [String: AnyObject], let jsonData = JSONValue.fromObject(jsonObject as AnyObject)?["data"]?.array{
+            for foodJSON in jsonData {
+                if let foodJSON = foodJSON.array, let food = Food.fromJSON(foodJSON){
+                    foods.append(food)
+                }
+            }
+        }
+        
+    }
+    
+    func checkLocationAuthorizationStatus(){
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+}
---
+++ Safe Reach/VCMapView.swift
@@ -0,0 +1,40 @@
+//
+//  VCMapView.swift
+//  Safe Reach
+//
+//  Created by Gina De La Rosa on 9/6/16.
+//  Copyright © 2016 Gina De La Rosa. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+extension MapViewController: MKMapViewDelegate {
+   
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if let annotation = annotation as? Food {
+            let identifier = "Pin"
+            var view: MKPinAnnotationView
+            if let dequeueView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView{
+                dequeueView.annotation = annotation
+                view = dequeueView
+            }else{
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.canShowCallout = true
+                view.rightCalloutAccessoryView = UIButton(type: UIButtonType.detailDisclosure) as UIView
+                view.pinTintColor = annotation.pinColor()
+            }
+            return view
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+       
+        let location = view.annotation as! Food
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        location.mapItem().openInMaps(launchOptions: launchOptions)
+    }
+}
