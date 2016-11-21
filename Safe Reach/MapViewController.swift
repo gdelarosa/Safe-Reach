482d87ebb220bb7791323de6844d03ea06b70cbb
Gina De La Rosa <g26delarosa@gmail.com>
2016-11-20 16:01:05 -0800

    Progress with searchbar

--- Safe Reach/MapViewController.swift
+++ Safe Reach/MapViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 import MapKit
 
+
+protocol HandleMapSearch: class {
+    func dropPinZoomIn(_ placemark:MKPlacemark)
+}
+
 class MapViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
     var locations = [Locations]()
     var locationManager = CLLocationManager()
+    // Testing
+    var selectedPin: MKPlacemark?
+    var resultSearchController: UISearchController!
     
     
     override func viewDidLoad() {
@@ -30,6 +38,20 @@ class MapViewController: UIViewController, UISearchBarDelegate {
         //centerMapOnLocation(initialLocation)
         loadInitialData()
         mapView.addAnnotations(locations)
+        
+        //Testing 
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        //resultSearchController.searchResultsUpdater = locationSearchTable
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        resultSearchController.hidesNavigationBarDuringPresentation = false
+        resultSearchController.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+       // locationSearchTable.mapView = mapView
+        //locationSearchTable.handleMapSearchDelegate = self
         
         
 
@@ -126,4 +148,50 @@ extension MapViewController : CLLocationManagerDelegate {
     }
     
 }
+
+//extension MapViewController: HandleMapSearch {
+//    
+//    func dropPinZoomIn(_ placemark: MKPlacemark){
+//        // cache the pin
+//        selectedPin = placemark
+//        // clear existing pins
+//        mapView.removeAnnotations(mapView.annotations)
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = placemark.coordinate
+//        annotation.title = placemark.name
+//        
+//        if let city = placemark.locality,
+//            let state = placemark.administrativeArea {
+//            annotation.subtitle = "\(city) \(state)"
+//        }
+//        
+//        mapView.addAnnotation(annotation)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+//        mapView.setRegion(region, animated: true)
+//    }
+//    
+//}
+
+//extension MapViewController : MKMapViewDelegate {
+//    
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+//        
+//        guard !(annotation is MKUserLocation) else { return nil }
+//        
+//        let reuseId = "pin"
+//        guard let pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView else { return nil }
+//        
+//        pinView.pinTintColor = UIColor.orange
+//        pinView.canShowCallout = true
+//        let smallSquare = CGSize(width: 30, height: 30)
+//        let button = UIButton(frame: CGRect(origin: CGPoint.zero, size: smallSquare))
+//        //button.setBackgroundImage(UIImage(named: "car"), for: UIControlState())
+//        //button.addTarget(self, action: #selector(MapViewController.getDirections), for: .touchUpInside)
+//        pinView.leftCalloutAccessoryView = button
+//        
+//        return pinView
+//}
+//}
+
 
